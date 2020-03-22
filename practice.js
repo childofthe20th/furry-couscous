575af123364555bcda71b04ba2f20a8b8b49b555
Eric Sanchez <bluemojo82@gmail.com>
2020-03-22 00:35:12 -0400

    blah

---
+++ practice.js
@@ -0,0 +1,63 @@
+// console.log("test");
+
+// let number = 0;
+// while (number <= 12) {
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// let result = 1;
+// let counter = 0;
+// while (counter < 10) {
+//     result = result * 2;
+//     counter = counter + 1;
+// }
+
+// console.log(result);
+
+// Week 3
+
+let arrDaysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+
+
+// let objDaysOfTheWeek = {
+//     day1: "Sunday",
+//     day2: "Monday",
+//     day3: "Tuesday",
+//     day4: "Wednesday",
+//     day5: "Thursday",
+//     day6: "Friday",
+//     day7: "Saturday"
+//     specialDayFunction: function() {
+//         let hoursOfTheDay = {
+//             0: "midnight",
+//             1: "one am"
+//         }
+//         return ("this is the function inside the object")
+//     }
+// }
+
+let demoFunction = () => {
+    // console.log(arrDaysOfTheWeek);
+    console.log(arrDaysOfTheWeek.length);
+
+    for(let i=0; i<arrDaysOfTheWeek.length; i++){
+        arrDaysOfTheWeek[i]
+    }
+    
+    
+}
+
+demoFunction()
+
+
+
