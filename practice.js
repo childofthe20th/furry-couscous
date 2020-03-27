4bdd856c16ee3ea5d8227a57fb7dc833d07972e4
Eric Sanchez <bluemojo82@gmail.com>
2020-03-27 00:24:06 -0400

    added more exercises

--- practice.js
+++ practice.js
@@ -209,6 +209,22 @@ console.log(countBs("BBC"));
 
 
 //Chapter Four: Objects and Arrays (Data Sets)
+console.log("***next chapter");
+
+//Methods
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+//push, pop, etc methods
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+sequence.pop();
+console.log(sequence);
+
+
 
 
 
