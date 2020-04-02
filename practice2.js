0e155d2abb44bc22965130a6a406c94eb490a991
Eric Sanchez <bluemojo82@gmail.com>
2020-04-02 02:04:40 -0400

    practice2.js created

---
+++ practice2.js
@@ -0,0 +1,15 @@
+//Chapter Four: Objects and Arrays (Data Sets)
+
+//Methods
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+//push, pop, etc methods
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+sequence.pop();
+console.log(sequence);
+
